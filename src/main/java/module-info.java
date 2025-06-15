ecd35612e7e8972de169bad892848c3e5b83ba2f
k.kenarskikh <k.kenarskikh@askona.ru>
2025-06-16 00:06:40 +0300

    Вторая лаба

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,9 @@ module com.example.demo {
     exports com.labs.lab1;
     exports com.labs.lab1.controller;
     opens com.labs.lab1.controller to javafx.fxml, javafx.graphics;
+
+    opens com.labs.lab2 to javafx.fxml, javafx.graphics;
+    exports com.labs.lab2;
+    exports com.labs.lab2.controller;
+    opens com.labs.lab2.controller to javafx.fxml, javafx.graphics;
 }

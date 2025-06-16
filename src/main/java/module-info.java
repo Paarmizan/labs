163753e49452a0f04500acfc3c1521ad922adf75
k.kenarskikh <k.kenarskikh@askona.ru>
2025-06-16 03:59:10 +0300

    Третья лабораторная

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
     opens com.labs.lab1 to javafx.fxml, javafx.graphics;
     exports com.labs.lab1;
@@ -11,4 +12,9 @@ module com.example.demo {
     exports com.labs.lab2;
     exports com.labs.lab2.controller;
     opens com.labs.lab2.controller to javafx.fxml, javafx.graphics;
+
+    opens com.labs.lab3 to javafx.fxml, javafx.graphics;
+    exports com.labs.lab3;
+    exports com.labs.lab3.controller;
+    opens com.labs.lab3.controller to javafx.fxml, javafx.graphics;
 }

f3999d701898fe83e35d5c24c090c403f3243c6c
k.kenarskikh <k.kenarskikh@askona.ru>
2026-01-25 01:54:23 +0300

    5 лаба

--- src/main/java/com/labs/lab5/controller/FifthLabController.java
+++ src/main/java/com/labs/lab5/controller/FifthLabController.java
@@ -2,8 +2,8 @@ package com.labs.lab5.controller;
 
 import com.labs.lab5.model.*;
 import com.labs.lab5.model.decorator.*;
-import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 
@@ -15,44 +15,38 @@ public class FifthLabController implements Initializable {
     public Pane paintPane;
     public Label statusLabel;
 
-    private VehicleComponent vehicle;
-    private VehicleComponent decoratedVehicle;
+    public CheckBox spoilerCheckBox;
+    public CheckBox headlightsCheckBox;
+
+    private VehicleComponent baseVehicle;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
-        // 1️⃣ создаём базовую машину
-        vehicle = new CarBase();
+        baseVehicle = new CarBase();
 
-        // 2️⃣ на старте декоров нет
-        decoratedVehicle = vehicle;
-
-        // 3️⃣ рисуем СРАЗУ
-        decoratedVehicle.operation(paintPane);
+        rebuildAndRedraw();
     }
 
-    public void addHeadlights(ActionEvent e) {
+    public void onOptionsChanged() {
+        rebuildAndRedraw();
+    }
+
+    private void rebuildAndRedraw() {
+
+        VehicleComponent decoratedVehicle = baseVehicle;
+
+        if (headlightsCheckBox.isSelected()) {
+            decoratedVehicle = new HeadlightsDecorator(decoratedVehicle);
+        }
+
+        if (spoilerCheckBox.isSelected()) {
+            decoratedVehicle = new SpoilerDecorator(decoratedVehicle);
+        }
+
         paintPane.getChildren().clear();
-
-        decoratedVehicle = (decoratedVehicle == null)
-                ? new HeadlightsDecorator(vehicle)
-                : new HeadlightsDecorator(decoratedVehicle);
-
-        redraw();
-    }
-
-    public void addSpoiler(ActionEvent e) {
-        paintPane.getChildren().clear();
-
-        decoratedVehicle = (decoratedVehicle == null)
-                ? new SpoilerDecorator(vehicle)
-                : new SpoilerDecorator(decoratedVehicle);
-
-        redraw();
-    }
-
-    private void redraw() {
         decoratedVehicle.operation(paintPane);
+
         statusLabel.setText(
                 decoratedVehicle.decorate() +
                         " | Стоимость: " + decoratedVehicle.cost()
